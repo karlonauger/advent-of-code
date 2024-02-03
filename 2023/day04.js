b537bc2a3fa3db5fa71be64fad0bc241c5b14601
Karlon Auger <karlonauger@gmail.com>
2024-02-03 18:03:46 -0500

    Cleanup Day4

--- 2023/day04.js
+++ 2023/day04.js
@@ -1,55 +1,45 @@
+/* eslint-disable no-underscore-dangle */
+
 'strict';
 
 const AOC = require('./AOC');
 
 AOC.setDay(4);
 
-AOC.part1(() => {
-  let total = 0;
+class Card {
+  constructor(line) {
+    const md = line.match(/Card\s*(\d+): (.*) \| (.*)/);
 
-  AOC.lines.forEach((line) => {
-    let lineTotal = 0;
-    const [winningNums, myNums] = line.split(': ')[1].split(' | ').map((nums) => nums.split(' ').filter((n) => n));
+    this.id = Number(md[1]);
+    this.winning = new Set(md[2].trim().split(/\s+/).map(Number));
+    this.numbers = new Set(md[3].trim().split(/\s+/).map(Number));
+    this.instances = 1;
+  }
 
-    winningNums.forEach((winningNum) => {
-      if (myNums.find((item) => item === winningNum)) {
-        lineTotal = lineTotal === 0 ? 1 : lineTotal * 2;
-      }
-    });
+  get matchSize() {
+    if (!this._matchSize) {
+      this._matchSize = [...this.winning].filter((num) => this.numbers.has(num)).length;
+    }
+    return this._matchSize;
+  }
 
-    total += lineTotal;
-  });
+  get points() {
+    return this.matchSize === 0 ? 0 : 2 ** (this.matchSize - 1);
+  }
+}
 
-  return total; // 21959
-});
+const cards = AOC.lines.map((line) => new Card(line));
+
+AOC.part1(() => cards.reduce((sum, card) => sum + card.points, 0)); // 21959
 
 AOC.part2(() => {
-  let total = 0;
-  const resultsArray = [];
+  cards.forEach((card, i) => {
+    const range = card.matchSize;
 
-  // Need to run in reverse to because later results accumulate up
-  AOC.lines.reverse().forEach((line) => {
-    let lineTotal = 1;
-    let totalWins = 0;
-
-    // Parse Line
-    const [winningNums, myNums] = line.split(': ')[1].split(' | ').map((nums) => nums.split(' ').filter((n) => n));
-
-    // Sum total matches
-    winningNums.forEach((winningNum) => {
-      if (myNums.find((item) => item === winningNum)) {
-        totalWins += 1;
-      }
+    cards.slice(i + 1, i + 1 + range).forEach((nextCard) => {
+      nextCard.instances += card.instances;
     });
-
-    // Sum wins of previous cards won
-    if (totalWins > 0) {
-      lineTotal += resultsArray.slice(totalWins * -1).reduce((acc, item) => acc + item);
-    }
-    resultsArray.push(lineTotal); // Record for later summing
-
-    total += lineTotal;
   });
 
-  return total; // 5132675
+  return cards.reduce((sum, card) => sum + card.instances, 0); // 5132675
 });
